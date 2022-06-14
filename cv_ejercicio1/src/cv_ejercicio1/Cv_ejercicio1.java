839adeca7994d0676585175e29414e7ea2ac7806
SantiagoB59 <75129125+SantiagoB59@users.noreply.github.com>
2022-06-14 17:03:39 -0500

    Update Cv_ejercicio1.java

--- cv_ejercicio1/src/cv_ejercicio1/Cv_ejercicio1.java
+++ cv_ejercicio1/src/cv_ejercicio1/Cv_ejercicio1.java
@@ -4,13 +4,8 @@ package cv_ejercicio1;
 public class Cv_ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("version 0");
         System.out.println("version 1");
-        System.out.println("version 2");
-        System.out.println("version 3");
-        //el nuevo version4, para hacer la mescla de desarrollador a el master 
-        System.out.println("version 4");
-        System.out.println("version 5");
+
     }
     
 }
